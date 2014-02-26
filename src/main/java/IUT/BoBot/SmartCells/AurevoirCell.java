31310d1431a0217529aa8d0632cff0a3c3751106
Ragnos <slydevis@hotmail.fr>
2014-02-26 09:04:35 +0100

    Add au revoir

---
+++ src/main/java/IUT/BoBot/SmartCells/AurevoirCell.java
@@ -0,0 +1,15 @@
+package IUT.BoBot.SmartCells;
+
+import IUT.BoBot.SmartCell;
+
+/**
+ * Created by guillaume on 26/02/14.
+ */
+public class AurevoirCell implements SmartCell {
+
+    public String ask(String question)
+    {
+        if(question.contains("au revoir")) return "au revoir";
+        else return null;
+    }
+}
---
+++ src/test/java/IUT/BoBot/SmartCells/AurevoirCellTest.java
@@ -0,0 +1,19 @@
+package IUT.BoBot.SmartCells;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AurevoirCellTest {
+
+    AurevoirCell cell = new AurevoirCell();
+
+    @Test
+    public void testAurevoir() throws Exception {
+        assertEquals("au revoir", cell.ask("au revoir"));
+    }
+
+    @Test
+    public void testNotAurevoir() throws Exception {
+        assertEquals(null, cell.ask("A la prochaine"));
+    }
+}
